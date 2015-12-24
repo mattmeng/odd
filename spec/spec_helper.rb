85abc6056bdc90dca9574fa097720fc2db983980
Matt Meng <mengmatt@gmail.com>
2015-12-24 11:23:41 -0700

    Create a test database directory for rspec.  Also added bindata to required gems.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,20 @@
 $LOAD_PATH.unshift File.expand_path( '../../lib', __FILE__ )
 require 'odd'
+require 'securerandom'
+require 'fileutils'
+
+RSpec.configure do |config|
+  config.before( :suite ) do
+    begin
+      dir = File.join( __dir__, SecureRandom.uuid )
+      unless File.directory?( dir )
+        Dir.mkdir( dir )
+        @database_dir = dir
+      end
+    end until @database_dir
+  end
+
+  config.after( :suite ) do
+    FileUtils.rm_rf( @database_dir )
+  end
+end
